777d691374482d4b0e9f3094b469329fd12f7aa4
Miguel Nixo <mnixo@nuxeo.com>
2020-06-09 19:01:29 +0100

    ELEMENTS-1187: automatic file update after grunt release

--- locale/ar-ly.js
+++ locale/ar-ly.js
@@ -1,11 +1,5 @@
 //! moment.js locale configuration
 
-;(function (global, factory) {
-   typeof exports === 'object' && typeof module !== 'undefined'
-       && typeof require === 'function' ? factory(require('../moment')) :
-   typeof define === 'function' && define.amd ? define(['../moment'], factory) :
-   factory(global.moment)
-}(this, (function (moment) { 'use strict';
 import moment from './';
 
 
